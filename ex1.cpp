1a9fbdc2c0547d12b066528eb97a2b597cd75b3c
unaaaaaaaaaa <100131900+unaaaaaaaaaa@users.noreply.github.com>
2022-03-15 20:29:06 +0800

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, product;
+    a = 20;
+    b = 7;
+
+ /*
+ 
+ x + y
+x - y
+x * y
+x / y
+x % y
+
+++x
+x++
+--x
+x--
+
+x = 16 
+x >> 1
+x >> 2
+x << 1
+ 
+ */
+ 
+ 
+    // Calculating product
+    product = a + b;
+ 
+    printf("a + b = %d \n", product);
+    
+    
+    // Calculating product
+    product = a - b;
+ 
+    printf("a - b = %d \n", product);
+    
+    // Calculating product
+    product = a * b;
+ 
+    printf("a * b = %d \n", product);
+    
+    // Calculating product
+    product = a / b;
+ 
+    printf("a / b = %d \n", product);
+    
+    // Calculating product
+    product = a * b;
+ 
+    printf("Product = %d \n", product);
+    
+    return 0;
+}
